535c69316c7ef62ba5cb4cc8b016f38c5ba23190
Juan Rodríguez <juan.rodriguezpedreira@gmail.com>
2025-03-31 18:16:19 +0000

    Set up seerver on port 5555

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { PORT } from "./config.js"
+
+const app = express();
+
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send(`Server is up running on ${PORT}`)
+});
+
+app.listen(PORT, () => {
+    console.log(`App is listening to port: ${PORT}`);
+});
+
+
+
