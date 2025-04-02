35388e37fe1b2235ed577a9a48c84ec97d4fc29b
Juan Rodríguez <juan.rodriguezpedreira@gmail.com>
2025-04-02 20:04:55 +0000

    Backend finished

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,5 @@
 export const PORT = 5555;
+
+export const mongoDBURL = "mongodb+srv://root:root@books-store-mern.65zdlku.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN";
+
+// Create Free db and delete after tutorial
--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,39 @@
-import express from "express";
-import { PORT } from "./config.js"
+import express, { request, response } from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from './routes/booksRoutes.js';
+
 
 const app = express();
+
+// Middelware for parsing request body
+app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow ALL Origins with default cors(*)
+// app.use(cors(*));
+// Option 2: Allow custom Origins
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
 
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send(`Server is up running on ${PORT}`)
 });
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`);
+app.use('/books', booksRoute);
+
+mongoose.connect(mongoDBURL).then(() => {
+    console.log('Connected to Data Base');
+    app.listen(PORT, () => {
+        console.log(`App is listening to port: ${PORT}`);
+    });
+}).catch((error) => {
+    console.log(error);
 });
-
-
-
